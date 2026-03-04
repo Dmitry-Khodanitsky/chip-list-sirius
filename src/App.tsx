8ce421f3b89d58bab06d00645bd1c5fce9a1ea15
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2026-03-04 09:09:35 +0300

    feat: добавлен рендер ChipList в App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <h1>Привет</h1>
-}
+import { ChipList } from './components/ChipList'
+import { Chip } from './components/Chip'
+import { CHIPS_MOCK } from './mocks/chips'
 
-export default App
+export const App = () => {
+  return (
+    <ChipList
+      items={CHIPS_MOCK}
+      getKey={(item) => item.id}
+      renderItem={(item, isSelected, onClick) => (
+        <Chip label={item.label} isSelected={isSelected} onClick={onClick} />
+      )}
+    />
+  )
+}
