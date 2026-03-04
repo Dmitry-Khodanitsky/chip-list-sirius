8ee45c7b13a44116fa1e6acdac8f26670125f7ba
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2026-03-04 09:12:28 +0300

    test: добавлены моки для чипов, это данные которые используются в ChipList для отрисовки списка чипов

---
+++ src/mocks/chips.ts
@@ -0,0 +1,23 @@
+export interface ChipItem {
+  id: string;
+  label: string;
+}
+// имитация данных для chips
+export const CHIPS_MOCK: ChipItem[] = [
+  { id: '1', label: 'Чипс 1' },
+  { id: '2', label: 'Чипс 2' },
+  { id: '3', label: 'Чипс 3' },
+  { id: '4', label: 'Чипс 4' },
+  { id: '5', label: 'Чипс 5' },
+  { id: '6', label: 'Чипс 6' },
+  { id: '7', label: 'Чипс 7' },
+  { id: '8', label: 'Чипс 8' },
+  { id: '9', label: 'Чипс 9' },
+  { id: '10', label: 'Чипс 10' },
+  { id: '11', label: 'Чипс scdsscsdcaccdcknjkacdkjad11' },
+  { id: '12', label: 'Чипс 12' },
+  { id: '13', label: 'Чипс 13' },
+  { id: '14', label: 'Чипс 14' },
+  { id: '15', label: 'Чипс 15' },
+  { id: '16', label: 'Чипс 16' },
+];
